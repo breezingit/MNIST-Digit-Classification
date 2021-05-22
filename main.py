2b35b5245f888efeb3f376459afe02c7fee8f46e
Yash Priyadarshi <yashpriyadarshi465@gmail.com>
2021-05-22 19:49:51 +0530

    vscode

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+# %%
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+# %%
 def unpickle(file):
     import pickle
     with open(file, 'rb') as fo:
@@ -10,22 +11,17 @@ def unpickle(file):
 
 dataset=unpickle('data_batch_1')
 
+# %%
 Y=[dataset[b'labels']]
 Y=np.transpose(Y)
 Y = np.asmatrix(Y)
 
+# %%
 X=[dataset[b'data']]
 X=np.transpose(X)
 X = np.asmatrix(X)
 X=np.transpose(X)
 
 m=len(X) #num of training examples
-
+# %%
 np.random.permutation(m)
-
-
-
-
-
-
-
