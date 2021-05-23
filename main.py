655882833d619302ad2915f1b537cd5f3ec04e28
Yash Priyadarshi <yashpriyadarshi465@gmail.com>
2021-05-23 16:27:51 +0530

    added function for randomly initialising weights

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # %%
 import numpy as np
 import matplotlib.pyplot as plt
-
+import randInitialise
 # %%
 def unpickle(file):
     import pickle
@@ -17,11 +17,19 @@ Y=np.transpose(Y)
 Y = np.asmatrix(Y)
 
 # %%
+num_row,num_col=X.shape
 X=[dataset[b'data']]
 X=np.transpose(X)
 X = np.asmatrix(X)
 X=np.transpose(X)
+m=num_row #num of training examples
 
-m=len(X) #num of training examples
 # %%
-np.random.permutation(m)
+new_Col=np.zeros((1, m))
+new_Col=np.transpose(new_Col)
+# %%
+X_ones=np.insert(X, 0, 1, axis=1)
+# %%
+##########  number of neurons in hidden layer==20
+initial_Theta1=randInitialiseWeights()
+# %%
